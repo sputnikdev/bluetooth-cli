b3ca49f68b3fc0a98e7c4fec5b98183c4d71a259
Vlad Kolotov <vkolotov@gmail.com>
2017-10-04 08:56:48 +1300

    updating install-dependencies scripts to remove tinyb installation

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dfile=lib/tinyb.jar -DgroupId=intel-iot-devkit -DartifactId=tinyb -Dversion=0.6.0 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/com.zsmartsystems.bluetooth.bluegiga-1.0.0-SNAPSHOT.jar -DgroupId=com.zsmartsystems.bluetooth.bluegiga -DartifactId=com.zsmartsystems.bluetooth.bluegiga -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
